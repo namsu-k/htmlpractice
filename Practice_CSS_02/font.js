65391903f96fb52720de9225b299cf70c444003f
southu <nghokr1111@gmail.com>
2023-05-10 17:40:55 +0900

    gekko v2.3

---
+++ Practice_CSS_02/font.js
@@ -0,0 +1,10 @@
+const cuts = document.getElementsByClassName("cut");
+
+console.log(cuts);
+console.log(cuts[0]);
+
+cuts[0].classList = "cut 1 on";
+console.log(cuts[0]);
+
+cuts[0].classList = "cut 1 off";
+console.log(cuts[0]);
